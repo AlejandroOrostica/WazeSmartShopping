a6e72afeff2a2bbb63999837f27aef196521b226
Hug0x <hgo.saavedra@gmail.com>
2019-10-16 14:57:17 -0300

    Cambios en la estructura de la App

--- mapas/lib/main.dart
+++ mapas/lib/main.dart
@@ -1,118 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_side_menu/screens/main_menu.dart';
 
-void main() => runApp(MaterialApp(
-  title: 'Prueba',
-  home: Map1(),
-));
+void main() => runApp(StartApp());
 
-class Map1 extends StatelessWidget{
+class StartApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Hola',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Mapa piso 1'),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(
-                Icons.arrow_forward),
-                onPressed: (){
-                  Navigator.push(context,MaterialPageRoute(builder: (context) => Map2()),
-                  );
-                } ,
-            ),
-          ],
-        ),
-        body: Stack(
-          children: <Widget>[
-            Container(
-              child: Image(
-                image: AssetImage('assets/mapa_flc_p1_v1_sm.jpg'),
-                fit: BoxFit.fill,
-                height: 1000,
-              ),
-            )
-          ],
-        )  
-      ),
+      debugShowCheckedModeBanner: false,
+      title: "waze Smart Shopping App",
+      home: MainMenu(),
     );
-
-    
-  }
-}
-
-class Map2 extends StatelessWidget{
-  @override 
-  Widget build(BuildContext context){
-    return MaterialApp(
-      title: 'Hola',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Mapa piso 2'),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(
-                Icons.arrow_forward),
-                onPressed: (){
-                  Navigator.push(context,MaterialPageRoute(builder: (context) => Map3()),
-                  );
-                } ,
-                
-            ),
-          ],
-        ),
-        body: Stack(
-          children: <Widget>[
-            Container(
-              child: Image(
-                image: AssetImage('assets/mapa_flc_p2_v1_sm.jpg'),
-                fit: BoxFit.fill,
-                height: 1000,
-              ),
-            )
-          ],
-        )  
-      ),
-    );
-
-    
-  }
-}
-
-class Map3 extends StatelessWidget{
-  @override 
-  Widget build(BuildContext context){
-    return MaterialApp(
-      title: 'Hola',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Mapa piso 3'),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(
-                Icons.arrow_forward),
-                onPressed: (){
-                  Navigator.pop(context);
-                },
-                
-            ),
-          ],
-        ),
-        body: Stack(
-          children: <Widget>[
-            Container(
-              child: Image(
-                image: AssetImage('assets/mapa_flc_p3_v1_sm.jpg'),
-                fit: BoxFit.fill,
-                height: 1000,
-              ),
-            )
-          ],
-        )  
-      ),
-    );
-
-    
   }
 }
